cfac08da147a7669548cf5247d6aed1afc6ef52b
bhattarairazu <razubhattarai87@gmail.com>
2017-01-17 18:21:13 +0545

    New Star Sequence Code

---
+++ sequence1/starsequence1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int i,j;
+    for(j=1;j<=5;j++){
+            for(i=1;i<=j;i++){
+    printf("*");
+
+            }
+             printf("\n");
+    }
+
+    return 0;
+}
